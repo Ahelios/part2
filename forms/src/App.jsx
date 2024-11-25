f1a4b6b8a8706e21243da3910522aa6e9d1fe752
Ahelios <artursmedveds@gmail.com>
2024-11-25 19:46:28 +0200

    2.11

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Persons from './components/Persons';
 import Personform from './components/Personform';
 import Search from './components/Search';
-import Note from './components/Note'
-import axios from 'axios'
+import axios from 'axios';
 
 function App() {
 	const [persons, setPersons] = useState([
@@ -12,51 +11,45 @@ function App() {
 		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
 		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
 	]);
-	
+
 	const [searchedValue, setSearchedValue] = useState('');
-	const [notes, setNotes] = useState([])
 
 	useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+		axios.get('http://localhost:3001/persons').then((response) => {
+			setPersons(response.data);
+			console.log(response.data);
+		});
+	}, []);
 
 	function handleSetPersons(newPerson) {
-
 		const nameExists = persons.some(
 			(person) =>
-				person.name === newPerson.name ||
-				person.number === newPerson.number
+				person.name === newPerson.name || person.number === newPerson.number
 		);
 
-    nameExists
-    ? alert(`${newPerson.name} is already added to phonebook`)
-    : setPersons(persons.concat({
-        ...newPerson,
-        id: persons[persons.length - 1].id + 1
-    }));
-    
+		nameExists
+			? alert(`${newPerson.name} is already added to phonebook`)
+			: setPersons(
+					persons.concat({
+						...newPerson,
+						id: persons[persons.length - 1].id + 1,
+					})
+			  );
 	}
 
-	function handleSetSearchedValue(value){
-		setSearchedValue(value)
+	function handleSetSearchedValue(value) {
+		setSearchedValue(value);
 	}
 
-  const filteredPersons = filterPersons(searchedValue)
+	const filteredPersons = filterPersons(searchedValue);
 
-	function filterPersons(value){
-    return persons.filter(
-      (person) =>
-        person.name.toLowerCase().includes(value) ||
-        person.number.includes(value)
-    );
-  }
+	function filterPersons(value) {
+		return persons.filter(
+			(person) =>
+				person.name.toLowerCase().includes(value) ||
+				person.number.includes(value)
+		);
+	}
 
 	return (
 		<div>
